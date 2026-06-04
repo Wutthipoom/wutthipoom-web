ebc109606269b2ebefb2e0f8e42c178a8d444945
Wutthipoom <bommomop@gmail.com>
2026-06-04 09:25:42 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+     <h2> ยินดีต้อนรับเข้าสู่เว็บไซส์</h2>
+      <p>โดย น้องนนท์คนดีคนเดิมเพิ่มใข่พิเศษ2ฟอง น้ำเปล่า 2 ขวด By นายวุฒิภูมิ วุฒิ</p>
+    </div>
+  );
+}
