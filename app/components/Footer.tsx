728544a6b8a4c5d210bd676782761d119b4eeb61
Wutthipoom <bommomop@gmail.com>
2026-06-04 10:28:37 +0700

    update

---
+++ app/components/Footer.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-8">        
+        <p>&copy; 2026 โดยวุฒิภูมิ วุฒิ. All rights reserved.</p>
+    </footer>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
-
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div >
+      <Navbar />
      <h2> ยินดีต้อนรับเข้าสู่เว็บไซส์</h2>
       <p>โดย น้องนนท์คนดีคนเดิมเพิ่มใข่พิเศษ2ฟอง น้ำเปล่า 2 ขวด By นายวุฒิภูมิ วุฒิ</p>
+      <Footer />
     </div>
   );
 }
